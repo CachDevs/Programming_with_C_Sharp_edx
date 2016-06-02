23daf1166ab4124d7b92d027123c70ec8031c1ca
CachDevs <CachDevs@gmail.com>
2016-06-02 19:56:12 +0200

    Module 1

---
+++ Module 1/Assignment/Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student Information
+            String fName = "First name";
+            String lName = "Last name";
+            String birth = "01/01/0000";
+            String addressl1 =  "Address line 1";
+            String addressl2 = "Address line 2";
+            String city = "City";
+            String state = "State";
+            int postal = 0000;
+            String country = "Country";
+
+            Console.WriteLine(fName + " " + lName + " " + birth + " " + addressl1 + " " + addressl2 + " " + city + " " + state + " " + postal + " " + country);
+            Console.ReadLine();
+        }
+    }
+}
