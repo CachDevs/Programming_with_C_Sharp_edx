1a839c909ebc96a762f1aba56177fd8978ed073f
CachDevs <CachDevs@gmail.com>
2016-06-10 18:37:33 +0200

    Module 5, solution 5.1

---
+++ Module 5/Classes/Classes/Point.cs
@@ -0,0 +1,45 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Point
+    {
+        // Variables
+        private int _x;
+        private int _y;
+        private static int objectCount;
+
+        // Default Constructor
+        public Point()
+        {
+            Console.WriteLine("Default Constructor called");
+            objectCount++;
+        }
+
+        // Non Default Constructor
+        public Point(int x, int y)
+        {
+            this._x = x;
+            this._y = y;
+            objectCount++;
+        }
+
+        // Method PrintCoords
+        public void PrintCoords()
+        {
+            Console.WriteLine("The point coordenates are x:{0}, y:{1}", this._x, this._y);
+        }
+
+        // Method ObjectCount
+        public static int ObjectCount()
+        {
+            return objectCount;
+        }
+    }
+}
---
+++ Module 5/Classes/Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point origin = new Point();
+            Point bottomRigth = new Point(1366, 768);
+            Console.WriteLine(Point.ObjectCount());
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
