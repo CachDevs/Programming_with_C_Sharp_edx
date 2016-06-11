f2b04573f1f4fb0e5bbb1f858b542044e597b91e
CachDevs <CachDevs@gmail.com>
2016-06-11 19:02:51 +0200

    Module 6, solution 6.1

---
+++ Module 6/Collector_Dispose/Collector_Dispose/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collector_Dispose 
+{
+    class Calculator : IDisposable
+    {
+
+        private bool disposed = false;
+
+        // Constructor
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created");
+        }
+
+        // public Divide Method
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+
+        // Destructor
+        ~Calculator()
+        {
+            this.Dispose();
+            Console.WriteLine("Calculator being finalized");
+        }
+
+        public void Dispose()
+        {
+            if(!disposed)
+                Console.WriteLine("Calculator being disposed");
+            this.disposed = true;
+            GC.SuppressFinalize(this);
+        }
+        
+    }
+}
