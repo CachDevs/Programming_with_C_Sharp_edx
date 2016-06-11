f8c8d40db2b600d53ec8384abd4e9c30bdb135e0
CachDevs <CachDevs@gmail.com>
2016-06-11 19:15:27 +0200

    Module 6, solution 6.2

--- Module 6/Vehicles/Vehicles/Program.cs
+++ Module 6/Vehicles/Vehicles/Program.cs
@@ -6,7 +6,27 @@ namespace Vehicles
     {
         static void doWork()
         {
-            // TODO:
+            Console.WriteLine("Journey by airplane");
+            Airplane myAirplane = new Airplane();
+            myAirplane.StartEngine("Contact");
+            myAirplane.TakeOff();
+            myAirplane.Drive();
+            myAirplane.Land();
+            myAirplane.StopEngine("Whirr");
+
+            Console.WriteLine("\nJourney by car");
+            Car myCar = new Car();
+            myCar.StartEngine("Brm brm");
+            myCar.Accelerate();
+            myCar.Drive();
+            myCar.Brake();
+            myCar.StopEngine("Phut phut");
+
+            Console.WriteLine("\nTesting polymorphsm");
+            Vehicles v = myCar;
+            v.Drive();
+            v = myAirplane;
+            v.Drive();
         }
 
         static void Main()
---
+++ Module 6/Vehicles/Vehicles/Vehicles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    class Vehicles
+    {
+        public void StartEngine(string noiseToMakeStarting)
+        {
+            Console.WriteLine("Starting engine: {0}", noiseToMakeStarting);
+        }
+
+        public void StopEngine(string noiseToMakeWhenStopping)
+        {
+            Console.WriteLine("Stopping engine: {0}", noiseToMakeWhenStopping);
+        }
+
+        public virtual void Drive()
+        {
+            Console.WriteLine("Default implementation of the drive method");
+        }
+    }
+}
